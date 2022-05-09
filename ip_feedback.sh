011a1344175750c291f250d3fdc457c4d7ada12b
slowrunner <slowrunner@users.noreply.github.com>
2022-05-09 11:00:17 -0400

    ch user from ubuntu to pi

--- ip_feedback.sh
+++ ip_feedback.sh
@@ -12,15 +12,15 @@ do
     sleep 1;
     IPs=$(hostname --all-ip-addresses)
     COUNT=$((COUNT+1))
-    echo "count: "$COUNT > /home/$USER/ipcount
+    echo "count: "$COUNT > /home/pi/ipcount
 done
 
 echo "done looping "
 
-echo "count: "$COUNT > /home/$USER/ipcount
+echo "count: "$COUNT > /home/pi/ipcount
 
-ifconfig wlan0 | grep 'inet ' | awk '{print $2}'  > /home/$USER/ip.number
-read -r IP_NUMBER < /home/$USER/ip.number
+ifconfig wlan0 | grep 'inet ' | awk '{print $2}'  > /home/pi/ip.number
+read -r IP_NUMBER < /home/pi/ip.number
 echo $IP_NUMBER
 
 # remove previous IP info
@@ -28,7 +28,7 @@ sudo rm /boot/*.assigned_ip &>/dev/null
 # sudo rm /home/pi/Desktop/*.assigned_ip &>/dev/null
 
 # remove previous Failed IP
-FAILED=/home/$USER/failedIP
+FAILED=/home/pi/failedIP
 if test -f "$FAILED"; then
     sudo rm $FAILED &>/dev/null
 fi
@@ -41,14 +41,14 @@ then
 #       echo $IP_NUMBER > /home/pi/Desktop/$IP_NUMBER.assigned_ip
         echo "IP info saved"
 
-        su -c "espeak-ng 'WiFi IP'" ubuntu
-        su -c "espeak-ng $IP_NUMBER" ubuntu
-        su -c "espeak-ng repeating "  ubuntu
-        su -c "espeak-ng $IP_NUMBER" ubuntu
+        su -c "espeak-ng 'WiFi IP'" pi
+        su -c "espeak-ng $IP_NUMBER" pi
+        su -c "espeak-ng repeating "  pi
+        su -c "espeak-ng $IP_NUMBER" pi
 else
 #       espeak-ng "no IP number"
         echo "no IP number"
-        echo "no IP" > /home/$USER/failedIP
+        echo "no IP" > /home/pi/failedIP
 #       echo "no IP" > /home/pi/Desktop/failedIP
 
 fi
